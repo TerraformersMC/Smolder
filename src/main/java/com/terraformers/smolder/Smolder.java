6654f59121b417a6ffed6fd91b39548f3b7aa7d7
paulevsGitch <paulevs@yandex.ru>
2020-07-19 22:06:31 +0300

    Fixed biome registration

--- src/main/java/com/terraformers/smolder/Smolder.java
+++ src/main/java/com/terraformers/smolder/Smolder.java
@@ -8,29 +8,25 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biome.Category;
 
-public class Smolder implements ModInitializer
-{
+public class Smolder implements ModInitializer {
+
 	public static final String MOD_ID = "smolder";
 	
 	@Override
-	public void onInitialize()
-	{
+	public void onInitialize() {
 		SmolderBiomeRegistry.init();
 		Registry.register(Registry.BIOME_SOURCE, new Identifier(MOD_ID, "smolder_biome_source"), SmolderBiomeSource.CODEC);
-		RegistryEntryAddedCallback.event(Registry.BIOME).register((i, id, biome) -> { SmolderBiomeRegistry.registerBiome(biome); });
-		registerExistingBiomes();
+		RegistryEntryAddedCallback.event(Registry.BIOME).register((i, id, biome) -> addBiome(biome));
+		Registry.BIOME.forEach((biome) -> addBiome(biome));
 	}
 	
-	/**
-	 * Register existing biomes on library startup. Must be called once.
-	 */
-	private void registerExistingBiomes()
+	private void addBiome(Biome biome)
 	{
-		Registry.BIOME.forEach((biome) -> {
-			if (biome.getCategory() == Category.NETHER && !(biome instanceof SmolderBiome))
-				SmolderBiomeRegistry.registerBiome(biome);
-		});
+		if (biome.getCategory() == Category.NETHER && !(biome instanceof SmolderBiome)) {
+			SmolderBiomeRegistry.registerBiome(biome);
+		}
 	}
 }
